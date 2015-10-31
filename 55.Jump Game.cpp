5422c2937b770d376fab75e6e2d236dd622a395d
Dong Bo <dongbo555@foxmail.com>
2015-10-31 21:50:57 +0800

    the 55'th problem add the second method

--- 55.Jump Game.cpp
+++ 55.Jump Game.cpp
@@ -10,3 +10,14 @@ bool canJump(vector<int>& nums) {
 	}
 	return i==length;
 }
+
+//反向就解
+bool canJump(vector<int>& nums) {
+	int last = nums.size() - 1;
+	for (int i = nums.size() - 2; i >= 0; i--) {
+		if (i + nums[i] >= last) {
+			last = i;
+		}
+	}
+	return (last <= 0);
+}
