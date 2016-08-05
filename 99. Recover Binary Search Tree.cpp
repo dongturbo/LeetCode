289054edfa5b1605cce1d39aa487e0295d4f0ac9
dongbo <dongbo555@foxmail.com>
2016-08-05 16:08:27 +0800

    add second ways to solved the 99'th problem

--- 99. Recover Binary Search Tree.cpp
+++ 99. Recover Binary Search Tree.cpp
@@ -25,3 +25,45 @@ void recoverTree(TreeNode* root) {
     sort(queue.begin(),queue.end());  
     recovTree(root,queue);
 }
+
+/*O(n)空间的解法是开一个指针数组，中序遍历，将节点指针一次存放到数组里，然后寻找两处逆向的位置，先从前向后找第一个逆序的位置，然后从后往前
+找第二个逆序的位置，交换着两个指针的值。
+ 中序遍历一般需要用到栈，空间也是O(n),利用Morris中序遍历空间才是O(1)
+*/
+
+void recoverTree(TreeNode* root) {
+     pair<TreeNode*, TreeNode*> broken;
+     TreeNode* prev = nullptr;
+     TreeNode* cur = root;
+     while (cur != nullptr) {
+         if (cur->left == nullptr) {
+             detect(broken, prev, cur);
+             prev = cur;
+             cur = cur->right;
+         } else {
+             auto node = cur->left;
+             while (node->right != nullptr && node->right != cur)
+             node = node->right;
+             if (node->right == nullptr) {
+                 node->right = cur;
+                 //prev = cur; 
+                 cur = cur->left;
+             } else {
+                 detect(broken, prev, cur);
+                 node->right = nullptr;
+                 prev = cur;
+                 cur = cur->right;
+             }
+         }
+     }
+     swap(broken.first->val, broken.second->val);
+ }
+ void detect(pair<TreeNode*, TreeNode*>& broken, TreeNode* prev,
+     TreeNode* current) {
+     if (prev != nullptr && prev->val > current->val) {
+         if (broken.first == nullptr) {
+             broken.first = prev;
+         } 
+         broken.second = current;
+     }
+ }
