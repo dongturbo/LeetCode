30542a11f9f7e378d81802a0cd241cf6fcf31e03
dongbo <dongbo555@foxmail.com>
2016-08-06 23:27:40 +0800

    solved the 104'th problem

---
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,34 @@
+//利用栈迭代，栈的最大长度即为最大深度
+int maxDepth(TreeNode* root) {
+	stack<TreeNode*> stack;
+	stack.push(root);
+	if (root == NULL)
+		return 0;
+	int depth=0;
+	while (!stack.empty()){
+		TreeNode* temp = stack.top();
+		if (stack.size() > depth)
+			depth = stack.size();
+		if (temp->left){
+			stack.push(temp->left);
+		}
+		else if(temp->right){
+			stack.push(temp->right);
+		}
+		else{
+			stack.pop();
+			while (!stack.empty()){
+				if (stack.top()->right != NULL && stack.top()->right != temp){
+					stack.push(stack.top()->right);
+					break;
+				}			
+				else{
+					temp = stack.top();
+					stack.pop();
+				}
+					
+			}
+		}
+	}
+	return depth;
+}
