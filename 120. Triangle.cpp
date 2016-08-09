67f9b74a7371e096eb5414c6e25cb9c0ba873ed0
dongbo <dongbo555@foxmail.com>
2016-08-10 00:36:55 +0800

    solved the 120'th problem

---
+++ 120. Triangle.cpp
@@ -0,0 +1,22 @@
+//动态规划+滚动数组，找去其中的关系式，f(x,y)表示到x层y个元素的最小路径和，那个可得f(x,y)=min(f(x-1,y),f(x-1,y-1))+triangle[x][y]
+int minimumTotal(vector<vector<int>>& triangle) {
+    vector<int> minSum(triangle.size(), 0);
+	int length = triangle.size();
+	for (int i = 0; i<triangle.size(); i++){
+		for (int j = 0; j<i + 1; j++){
+			if (j == 0 && i!=0){;
+				minSum[length + j - i - 1] = minSum[length + j - i]+triangle[i][j];
+			}
+			else if (j == i){
+				minSum[length + j - i - 1] += triangle[i][i];
+			}
+			else{
+				minSum[length + j - i - 1] = min(minSum[length + j - i], minSum[length + j - i - 1]) + triangle[i][j];
+			}
+		}
+	}
+	int minTotal = minSum[0];
+	for (int i = 1; i<triangle.size(); i++)
+		minTotal = min(minTotal, minSum[i]);
+	return minTotal;
+}
