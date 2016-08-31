a5f121e1d589958204c888482f59bf57a027ae9f
dongbo <dongbo555@foxmail.com>
2016-08-31 21:52:56 +0800

    reconstruct code

--- 129. Sum Root to Leaf Numbers.cpp
+++ 129. Sum Root to Leaf Numbers.cpp
@@ -1,22 +1,12 @@
 //利用dfs递归遍历，然后把每次的值存储下来，最终计算出结果。
-void dfsSum(TreeNode* root, int temp, int &sum){
-  	if (root == nullptr)
-  		return;
-  	temp =temp*10+ root->val;
-  	if (root->left == nullptr && root->right == nullptr){
-  		sum += temp;
-  		return;
-  	}
-  	if (root->left){
-  		dfsSum(root->left, temp , sum);
-  	}
-  	if (root->right){
-  		dfsSum(root->right, temp, sum);
-  	}
-  }
-  
-  int sumNumbers(TreeNode* root) {
-  	int sum = 0, temp = 0;
-  	dfsSum(root, temp, sum);
-  	return sum;
-  }
+int sumNumbers(TreeNode *root) {
+	return dfs(root, 0);
+}
+
+int dfs(TreeNode *root, int sum) {
+	if (root == nullptr) return 0;
+	if (root->left == nullptr && root->right == nullptr)
+		return sum * 10 + root->val;
+	return dfs(root->left, sum * 10 + root->val) +
+		dfs(root->right, sum * 10 + root->val);
+}
