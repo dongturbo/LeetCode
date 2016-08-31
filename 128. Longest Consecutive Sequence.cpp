cf9a3a35d742b6948e922a3e2a382fef6891143d
dongbo <dongbo555@foxmail.com>
2016-08-31 20:59:21 +0800

    add the second way to solve this problem

--- 128. Longest Consecutive Sequence.cpp
+++ 128. Longest Consecutive Sequence.cpp
@@ -18,3 +18,30 @@ int longestConsecutive(vector<int>& nums) {
 	}
 	return maxLen;
 }
+
+//利用union，find操作，连续序列可以用两端和长度来表示。用unordered_map<int,int> map,来存储
+int longestConsecutive(vector<int> &nums) {
+	unordered_map<int, int> map;
+	int size = nums.size();
+	int l = 1;
+	for (int i = 0; i < size; i++) {
+		if (map.find(nums[i]) != map.end()) continue;
+		map[nums[i]] = 1;
+		if (map.find(nums[i] - 1) != map.end()) {
+			l = max(l, mergeCluster(map, nums[i] - 1, nums[i]));
+		}
+		if (map.find(nums[i] + 1) != map.end()) {
+			l = max(l, mergeCluster(map, nums[i], nums[i] + 1));
+		}
+	}
+	return size == 0 ? 0 : l;
+}
+
+int mergeCluster(unordered_map<int, int> &map, int left, int right) {
+	int upper = right + map[right] - 1;
+	int lower = left - map[left] + 1;
+	int length = upper - lower + 1;
+	map[upper] = length;
+	map[lower] = length;
+	return length;
+}
