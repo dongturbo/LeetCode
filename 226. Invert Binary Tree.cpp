727388404d9552bb841ea16c1adc6718e63a4fd0
dongbo <dongbo555@foxmail.com>
2016-09-01 19:31:40 +0800

    Create 226. Invert Binary Tree.cpp

---
+++ 226. Invert Binary Tree.cpp
@@ -0,0 +1,10 @@
+TreeNode* invertTree(TreeNode* root) {
+    if(root==nullptr)
+        return root;
+    TreeNode* temp=root->left;
+    root->left=root->right;
+    root->right=temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
