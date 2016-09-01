c6e447ed25d9d6eb60bbbb941f214d54b2a0b25f
dongbo <dongbo555@foxmail.com>
2016-09-01 16:41:26 +0800

    add the second way to solved the 136'th problem

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -11,3 +11,11 @@ int singleNumber(vector<int>& nums) {
 	}
 	return 2 * singleSum - sum;
 }
+
+//利用异或，因为出现两次的异或为0，所以只要把所有都异或一遍剩余的数即为只出现一次的数据
+int singleNumber(vector<int>& nums) {
+    int result=nums[0];
+    for(int i=1;i<nums.size();i++)
+        result^=nums[i];
+    return result;
+}
