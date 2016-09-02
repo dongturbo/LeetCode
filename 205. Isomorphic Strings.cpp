764b1856815e3e29ced2b9790e1a572f4a31f80e
dongbo <dongbo555@foxmail.com>
2016-09-02 23:16:16 +0800

    Update 205. Isomorphic Strings.cpp

--- 205. Isomorphic Strings.cpp
+++ 205. Isomorphic Strings.cpp
@@ -26,3 +26,17 @@ bool isIsomorphic(string s, string t) {
 	}
 	return true;
 }
+//第二种解法，参考别人的
+bool isIsomorphic(string s, string t) {
+    int cs[128] = {0}, ct[128] = {0};
+    for(int i=0; i<s.size(); i++)
+    {
+        if(cs[s[i]] != ct[t[i]]) return false;
+        else if(!cs[s[i]] && !ct[t[i]]) //only record once
+        {
+            cs[s[i]] = i+1;
+            ct[t[i]] = i+1;
+        }
+    }
+    return true;
+}
