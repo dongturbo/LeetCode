37108d29f301e6c514157ca4a9bf3204a63a5904
dongbo <dongbo555@foxmail.com>
2016-09-02 15:21:50 +0800

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,11 @@
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> set;
+    for(int i=0;i<nums.size();i++){
+        if(set.find(nums[i])==set.end()){
+            set.insert(nums[i]);
+        }else{
+            return true;
+        }
+    }
+    return false;
+}
