e694d7ecbda7a65f6177f6597e4aa3f947c93dd5
dongbo <dongbo555@foxmail.com>
2016-09-04 23:47:35 +0800

    Update 1.Two Sum.cpp

--- 1.Two Sum.cpp
+++ 1.Two Sum.cpp
@@ -2,18 +2,18 @@ vector<int> twoSum(vector<int>& nums, int target) {
         int len = nums.size();  
         assert(len >= 2);             
         vector<int> ret(2, 0);  
-        map<int, int> mapping;              // default all are 0  
+        map<int, int> mapping;              
         vector<long long> mul(len, 0);  
         for(int i = 0; i < len; i++){  
             mul[i] = (target - nums[i]) * nums[i];  
-            if(mapping[mul[i]] > 0){        // not default 0  
+            if(mapping[mul[i]] > 0){
                 if(nums[i] + nums[mapping[mul[i]] - 1] == target){  
                     ret[0] = mapping[mul[i]];  
                     ret[1] = i + 1;  
                     break;  
                 }  
             } else {  
-                mapping[mul[i]] = i + 1;    // larger than 0  
+                mapping[mul[i]] = i + 1;   
             }  
         }  
         return ret;  
